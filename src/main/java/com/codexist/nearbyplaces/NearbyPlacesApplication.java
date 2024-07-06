ddef0a137ab4f5c1cca0f059b9d8aa2329929b02
Soner <syln.soner@gmail.com>
2024-07-06 11:41:50 +0300

    Initialize NearbyPlaces Spring Boot Maven project

---
+++ src/main/java/com/codexist/nearbyplaces/NearbyPlacesApplication.java
@@ -0,0 +1,13 @@
+package com.codexist.nearbyplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyPlacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearbyPlacesApplication.class, args);
+    }
+
+}
