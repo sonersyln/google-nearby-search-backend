9a21ee44d81c9b0357f1347a5d1c273cd4ffe7dd
Soner <syln.soner@gmail.com>
2024-07-07 15:08:46 +0300

    Completed Nearby Places Finder project

--- src/main/java/com/codexist/nearbyplaces/config/WebConfig.java
+++ src/main/java/com/codexist/nearbyplaces/config/WebConfig.java
@@ -1,23 +1,18 @@
 package com.codexist.nearbyplaces.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization");
+
+
     }
 }
